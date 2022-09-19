83da3a6d4b98d7fa79e8d15d5ecff2a7deea4af1
yulydey <yulyde1@hotmail.com>
2022-09-19 17:59:27 -0500

    Se crea clase Car, se agregan datos

---
+++ Java/Car.java
@@ -0,0 +1,11 @@
+
+class Car {
+    Integer id;
+    String license;
+    String driver;
+    Integer passegenger;
+
+    void printDatacar() {
+        System.out.println("License:" + license + "   driver:" + driver);
+    }
+}
