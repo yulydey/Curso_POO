49bed90e26c2a1f3754dc46fd2f50337e51edebd
yulydey <yulyde1@hotmail.com>
2022-09-19 17:40:11 -0500

    Se crea clase Main, de proyector Uber y se crean datos

---
+++ Java/Main.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("HOLA JAVA ++30 AÑOS");
+        Car car = new Car();
+        car.license = "XYS39E";
+        car.driver = "FELIPE ARANGO";
+        car.passegenger = 7;
+        car.printDatacar();
+
+        Car car2 = new Car();
+        car2.license = "JYF43F";
+        car2.driver = "JULIAN COLINA";
+        car2.passegenger = 4;
+        car2.printDatacar();
+    }
+}
